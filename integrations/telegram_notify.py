f537319dbb1d338beaee42941a27f3c708f614d7
funmicra <el.funmicra@gmail.com>
2026-02-25 23:55:05 +0200

    change discord webhook url

--- integrations/telegram_notify.py
+++ integrations/telegram_notify.py
@@ -1,52 +1,3 @@
-# import os
-# import sys
-# import requests
-
-# BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-# CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
-# if not BOT_TOKEN or not CHAT_ID:
-#     raise RuntimeError("Missing Telegram credentials in environment variables")
-
-# def send_telegram_message(message: str):
-#     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-#     payload = {
-#         "chat_id": CHAT_ID,
-#         "text": message,
-#         "parse_mode": "Markdown"
-#     }
-
-#     response = requests.post(url, json=payload, timeout=10)
-#     response.raise_for_status()
-
-
-# if __name__ == "__main__":
-#     """
-#     Expected args:
-#       argv[1] -> pipeline name
-#       argv[2] -> build number
-#       argv[3] -> build status (SUCCESS / FAILURE)
-#       argv[4] -> build URL
-#     """
-
-#     pipeline = sys.argv[1]
-#     build_number = sys.argv[2]
-#     status = sys.argv[3]
-#     build_url = sys.argv[4]
-
-#     emoji = "✅" if status == "SUCCESS" else "❌"
-
-#     message = (
-#         f"{emoji} *Jenkins Pipeline Finished*\n\n"
-#         f"*Pipeline:* {pipeline}\n"
-#         f"*Build:* #{build_number}\n"
-#         f"*Status:* {status}\n"
-#         f"*URL:* {build_url}"
-#     )
-
-#     send_telegram_message(message)
-
-
 #!/usr/bin/env python3
 import os
 import sys
@@ -61,7 +12,7 @@ logging.basicConfig(
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+CERT_AUTOMATION_DISCORD_WEBHOOK = os.getenv("CERT_AUTOMATION_DISCORD_WEBHOOK")
 
 if not BOT_TOKEN or not CHAT_ID:
     raise RuntimeError("Missing TELEGRAM_BOT_TOKEN or TELEGRAM_CHAT_ID")
@@ -101,7 +52,7 @@ def send_telegram(message: str):
 
 
 def send_discord(pipeline: str, build_number: str, status: str, build_url: str):
-    if not DISCORD_WEBHOOK_URL:
+    if not CERT_AUTOMATION_DISCORD_WEBHOOK:
         logging.info("No Discord webhook configured. Skipping.")
         return
 
@@ -118,7 +69,7 @@ def send_discord(pipeline: str, build_number: str, status: str, build_url: str):
 
     payload = {"embeds": [embed]}
 
-    r = requests.post(DISCORD_WEBHOOK_URL, json=payload, timeout=10)
+    r = requests.post(CERT_AUTOMATION_DISCORD_WEBHOOK, json=payload, timeout=10)
     r.raise_for_status()
     logging.info("Discord notification sent.")
 
